0b40c0400d4f758afd17d2d97af3798d03e9a612
Hao CN <caonguyenhao69@gmail.com>
2025-08-21 18:59:44 +0700

    Done Board Bar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -15,6 +15,8 @@ import Badge from '@mui/material/Badge'
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
 import Tooltip from '@mui/material/Tooltip'
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd'
+
 function AppBar() {
     return (
         <Box px={2} sx={{
@@ -42,7 +44,7 @@ function AppBar() {
                     <Recent />
                     <Starred />
                     <Templates />
-                    <Button variant="outlined">Create</Button>
+                    <Button variant="outlined" startIcon={<LibraryAddIcon />}>Create</Button>
                 </Box>
             </Box>
 
--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,11 +1,83 @@
 import Box from '@mui/material/Box'
+import Chip from '@mui/material/Chip'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
+import { Tooltip } from '@mui/material'
+import Button from '@mui/material/Button'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+const MENU_STYLES = {
+    color: 'primary.main',
+    bgcolor: 'white',
+    border: 'none',
+    paddingX: '5px',
+    borderRadius: '4px',
+    '& .MuiSvgIcon-root': {
+        color: 'primary.main'
+    },
+    '&:hover': {
+        bgcolor: 'primary.50'
+    }
+}
 
 function BoardBar() {
     return (
         <Box sx={{
-            backgroundColor: 'primary.dark', width: '100%', height: (theme) => theme.trello.boardBarHeight, display: 'flex', alignItems: 'center'
+            width: '100%',
+            height: (theme) => theme.trello.boardBarHeight,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            gap: 2,
+            paddingX: 2,
+            overflowX: 'auto',
+            borderTop: '1px solid #00bfa5'
         }}>
-            Board Bar
+            <Box sx={{ display: 'flex', alignItems: ' center', gap: 2 }}>
+                <Chip sx={MENU_STYLES}
+                    icon={<DashboardIcon />}
+                    label="Public/Private Workspace" clickable />
+                <Chip sx={MENU_STYLES}
+                    icon={<VpnLockIcon />}
+                    label="Public/Private Workspace" clickable />
+                <Chip sx={MENU_STYLES}
+                    icon={<AddToDriveIcon />}
+                    label="Add to Google Drive" clickable />
+                <Chip sx={MENU_STYLES}
+                    icon={<BoltIcon />}
+                    label="Automation" clickable />
+                <Chip sx={MENU_STYLES}
+                    icon={<FilterListIcon />}
+                    label="Filter" clickable />
+            </Box>
+            <Box sx={{ display: 'flex', alignItems: ' center', gap: 2 }}>
+                <Button variant="outlined" startIcon={<PersonAddIcon />}>Invite</Button>
+                <AvatarGroup max={7} sx={{
+                    '& .MuiAvatar-root': {
+                        width: 34,
+                        height: 34,
+                        fontSize: 16
+                    }
+                }}>
+                    <Tooltip title='HaoCN'>
+                        <Avatar alt="HaoCN" src="/static/images/avatar/1.jpg" />
+                    </Tooltip>
+                    <Tooltip title='T'>
+                        <Avatar alt="T" src="/static/images/avatar/1.jpg" />
+                    </Tooltip>
+                    <Tooltip title='D'>
+                        <Avatar alt="D" src="/static/images/avatar/1.jpg" />
+                    </Tooltip>
+                    <Tooltip title='L'>
+                        <Avatar alt="L" src="/static/images/avatar/1.jpg" />
+                    </Tooltip>
+                </AvatarGroup>
+            </Box>
         </Box>
     )
 }
