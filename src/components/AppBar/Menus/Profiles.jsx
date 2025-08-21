6f93288fb96b353d8822dbf7894fbd8290c4ac66
Hao CN <caonguyenhao69@gmail.com>
2025-08-21 21:18:52 +0700

    Customize interface

--- src/components/AppBar/Menus/Profiles.jsx
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -32,7 +32,7 @@ function Profiles() {
                     aria-haspopup="true"
                     aria-expanded={open ? 'true' : undefined}
                 >
-                    <Avatar sx={{ width: 30, height: 32 }} alt='Avatar' />
+                    <Avatar sx={{ width: 36, height: 36 }} alt='Avatar' />
                 </IconButton>
             </Tooltip>
             <Menu
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,3 @@
-import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 // Create a theme instance.
@@ -7,20 +6,7 @@ const theme = extendTheme({
         appBarHeight: '58px',
         boardBarHeight: '60px'
     },
-    colorSchemes: {
-        light: {
-            palette: {
-                primary: teal,
-                secondary: deepOrange
-            }
-        },
-        dark: {
-            palette: {
-                primary: cyan,
-                secondary: orange
-            }
-        }
-    },
+    colorSchemes: {},
     components: {
         MuiCssBaseline: {
             styleOverrides: {
@@ -30,53 +16,38 @@ const theme = extendTheme({
                         heigh: '8px'
                     },
                     '*::-webkit-scrollbar-thumb': {
-                        backgroundColor: '#bdc3c7',
+                        backgroundColor: '#dcdde1',
                         borderRadius: '8px'
                     },
                     '*::-webkit-scrollbar-thumb:hover': {
-                        backgroundColor: '#00b894'
+                        backgroundColor: 'white'
                     }
                 }
             }
         },
-        // Name of the component
         MuiButton: {
             styleOverrides: {
-                // Name of the slot
                 root: {
-                    // Some CSS
-                    textTransform: 'none'
+                    textTransform: 'none',
+                    borderWidth: '0.5px',
+                    '&:hover': { borderWidth: '0.5px' }
                 }
             }
         },
         MuiInputLabel: {
             styleOverrides: {
-                // Name of the slot
-                root: ({ theme }) => ({
-                    color: theme.palette.primary.main,
+                root: {
                     fontSize: '0.875rem'
-                })
+                }
             }
         },
         MuiOutlinedInput: {
             styleOverrides: {
-                // Name of the slot
-                root: ({ theme }) => {
-                    return {
-                        color: theme.palette.primary.main,
-                        fontSize: '0.875rem',
-                        '.MuiOutlinedInput-notchedOutline': {
-                            borderColor: theme.palette.primary.light
-                        },
-                        '&:hover': {
-                            '.MuiOutlinedInput-notchedOutline': {
-                                borderColor: theme.palette.primary.main
-                            }
-                        },
-                        '& fieldset': {
-                            borderWidth: '1px !important'
-                        }
-                    }
+                root: {
+                    fontSize: '0.875rem',
+                    '& fieldset': { borderWidth: '0.5px !important' },
+                    '&:hover fieldset': { borderWidth: '1px !important' },
+                    '&.Mui-focused fieldset': { borderWidth: '1px !important' }
                 }
             }
         }
