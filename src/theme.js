c496e1ef2ca14b36220ba5700384ccd45f63a7eb
Hao CN <caonguyenhao69@gmail.com>
2025-08-21 10:32:27 +0700

    Done AppBar

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,12 @@
+import { BorderColor } from '@mui/icons-material'
 import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 // Create a theme instance.
 const theme = extendTheme({
     trello: {
-        appBarHeight: '48px',
-        boardBarHeight: '58px'
+        appBarHeight: '58px',
+        boardBarHeight: '60px'
     },
     colorSchemes: {
         light: {
@@ -18,6 +19,66 @@ const theme = extendTheme({
             palette: {
                 primary: cyan,
                 secondary: orange
+            }
+        }
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: {
+                body: {
+                    '*::-webkit-scrollbar': {
+                        width: '8px',
+                        heigh: '8px'
+                    },
+                    '*::-webkit-scrollbar-thumb': {
+                        backgroundColor: '#bdc3c7',
+                        borderRadius: '8px'
+                    },
+                    '*::-webkit-scrollbar-thumb:hover': {
+                        backgroundColor: '#00b894'
+                    }
+                }
+            }
+        },
+        // Name of the component
+        MuiButton: {
+            styleOverrides: {
+                // Name of the slot
+                root: {
+                    // Some CSS
+                    textTransform: 'none'
+                }
+            }
+        },
+        MuiInputLabel: {
+            styleOverrides: {
+                // Name of the slot
+                root: ({ theme }) => ({
+                    color: theme.palette.primary.main,
+                    fontSize: '0.875rem'
+                })
+            }
+        },
+        MuiOutlinedInput: {
+            styleOverrides: {
+                // Name of the slot
+                root: ({ theme }) => {
+                    return {
+                        color: theme.palette.primary.main,
+                        fontSize: '0.875rem',
+                        '.MuiOutlinedInput-notchedOutline': {
+                            borderColor: theme.palette.primary.light
+                        },
+                        '&:hover': {
+                            '.MuiOutlinedInput-notchedOutline': {
+                                borderColor: theme.palette.primary.main
+                            }
+                        },
+                        '& fieldset': {
+                            borderWidth: '1px !important'
+                        }
+                    }
+                }
             }
         }
     }
